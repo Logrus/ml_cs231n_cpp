d0d3e79cf16d584dc8dbc2b161501443b9cd85cb
Vladislav Tananaev <v.d.tananaev@gmail.com>
2016-11-22 16:39:24 +0100

    rescale axis.

--- simplenet_ui.cpp
+++ simplenet_ui.cpp
@@ -350,8 +350,7 @@ void SimpleNetUI::on_pushButton_clicked()
             true_iter++;
 
             ui->lossplot->graph(0)->setData(iterations, loss_stats);
-            ui->lossplot->xAxis->setRange(0, *(iterations.end()-1));
-            ui->lossplot->yAxis->setRange(minloss, maxloss);
+            ui->lossplot->graph(0)->rescaleAxes();
             ui->lossplot->replot();
 
         }
@@ -375,8 +374,8 @@ void SimpleNetUI::on_pushButton_clicked()
 
         ui->accplot->graph(0)->setData(epochs,val_acc_stats);
         ui->accplot->graph(1)->setData(epochs,train_acc_stats);
-        ui->accplot->xAxis->setRange(0,  *(epochs.end()-1));
-        ui->accplot->yAxis->setRange(minacc, maxacc);
+        ui->accplot->graph(0)->rescaleAxes();
+        ui->accplot->graph(1)->rescaleAxes(true);
         ui->accplot->replot();
 
         if(classifier->saveWeights("weights.dat")){
